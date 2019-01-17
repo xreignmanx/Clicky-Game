bfa791ac8ccf8fb7a339566535c01349cf5fed22
xreignmanx <rtballentine@yahoo.com>
2019-01-17 09:50:25 -0500

    fixed app.js

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
   };
 
 
-shuffleCard = array.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
-  = id => {
-    // Filter this.state.cards for cards with an id not equal to the id being removed
-    const cards = this.state.cards.filter(card => card.id !== id);
-    // Set this.state.cards equal to the new cards array
-    this.setState({ cards });
-  };
+// shuffleCard = array.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
+//   = id => {
+//     // Filter this.state.cards for cards with an id not equal to the id being removed
+//     const cards = this.state.cards.filter(card => card.id !== id);
+//     // Set this.state.cards equal to the new cards array
+//     this.setState({ cards });
+//   };
 
   // Map over this.state.cards and render a Card component for each card object
   render() {
