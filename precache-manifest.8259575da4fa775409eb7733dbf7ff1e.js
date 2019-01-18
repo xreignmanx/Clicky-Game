67ca2a3a31d24f6994a6b871aa659e8d71d7dcbe
xreignmanx <rtballentine@yahoo.com>
2019-01-18 13:54:27 -0500

    Updates

---
+++ precache-manifest.8259575da4fa775409eb7733dbf7ff1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "44ae86921b991a693a0b",
+    "url": "/Clicky-Game/static/js/main.44ae8692.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "44ae86921b991a693a0b",
+    "url": "/Clicky-Game/static/css/main.9cfbeaf3.chunk.css"
+  },
+  {
+    "revision": "3584fa648582a1b822b1fce47bd99f9b",
+    "url": "/Clicky-Game/index.html"
+  }
+];
